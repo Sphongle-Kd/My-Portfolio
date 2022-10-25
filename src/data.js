bfb62b4c9a0fd05c8af9735ac0ecf345048bfdde
Sphongle-Kd <kamaldeopa56@gmail.com>
2022-10-26 01:47:26 +0530

    Projects update

--- src/data.js
+++ src/data.js
@@ -1,32 +1,32 @@
+import Screenshot from "../src/image/screenshot.png";
+import Portfolio from "../src/image/portfolio.png";
+import Blog from "../src/image/theCrazyBlogs.png";
+import Ecommerce from "../src/image/E-Commerce.png";
+
 export const projects = [
   {
     id: 1,
-    img: "screenshot.png",
-    link: "https://netflix-clone-e04aa.web.app/",
+    img: Portfolio,
+    link: "",
   },
   {
     id: 2,
-    img: "screenshot.png",
-    link: "https://netflix-clone-e04aa.web.app/",
+    img: "",
+    link: "",
   },
   {
     id: 3,
-    img: "screenshot.png",
+    img: Screenshot,
     link: "https://netflix-clone-e04aa.web.app/",
   },
   {
     id: 4,
-    img: "screenshot.png",
+    img: Ecommerce,
     link: "https://netflix-clone-e04aa.web.app/",
   },
   {
     id: 5,
-    img: "screenshot.png",
-    link: "https://netflix-clone-e04aa.web.app/",
-  },
-  {
-    id: 6,
-    img: "screenshot.png",
-    link: "https://netflix-clone-e04aa.web.app/",
+    img: Blog,
+    link: "https://the-crazy-blogs.web.app/",
   },
 ];
